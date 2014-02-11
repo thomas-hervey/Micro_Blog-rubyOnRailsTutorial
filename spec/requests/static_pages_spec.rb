5b4744af52bff516b995067cd98a682dfffc2ac4
Thomas Hervey <h46@umbc.edu>
2014-02-11 18:36:16 -0500

    Completed Ch.3 Ex. 1 & 2 (adding contact page)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe "Static pages" do
 	
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 	    it "should have the content 'Sample App' " do
 	    	visit '/static_pages/home'
 	    	expect(page).to have_content('Sample App')
 	    end
 	    
-	    it "should have the right title " do
+	    it "should have the right title 'Home' " do
 	    	visit '/static_pages/home'
 	    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
 	    end
@@ -21,7 +23,7 @@ describe "Static pages" do
 	    	expect(page).to have_content('Help')
 	    end
 
-	    it "should have the right title " do
+	    it "should have the right title 'Help' " do
 	    	visit '/static_pages/help'
 	    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 	    end
@@ -34,9 +36,22 @@ describe "Static pages" do
 	    	expect(page).to have_content('About Us')
 	    end
 
-	    it "should have the right title " do
+	    it "should have the right title 'About Us' " do
 	    	visit '/static_pages/about'
 	    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+	    end
+  	end
+
+
+  	describe "Contact page" do
+	    it "should have the content 'Contact' " do
+	    	visit '/static_pages/contact'
+	    	expect(page).to have_content('Contact')
+	    end
+
+	    it "should have the right title 'Contact' " do
+	    	visit '/static_pages/contact'
+	    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
 	    end
   	end
 
