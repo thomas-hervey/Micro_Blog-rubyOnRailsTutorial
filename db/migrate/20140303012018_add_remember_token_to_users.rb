010360ec443efe256c4bbd304fd3975377661c8d
Thomas Hervey <h46@umbc.edu>
2014-03-02 22:13:18 -0500

    db remove column added because heroku rake failed. will likely remove later

--- db/migrate/20140303012018_add_remember_token_to_users.rb
+++ db/migrate/20140303012018_add_remember_token_to_users.rb
@@ -1,5 +1,6 @@
 class AddRememberTokenToUsers < ActiveRecord::Migration
   def change
+    remove_column :users, :remember_token, :string
     add_column :users, :remember_token, :string
     add_index  :users, :remember_token
   end
