718e2f7fc0557bb135d6b1c23d30447bea59de7a
Thomas Hervey <h46@umbc.edu>
2014-03-09 21:50:08 -0400

    completed 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -41,8 +42,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    # @user is set in before_action
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      # handle a successful update
+      flash[:success] = 'Profile updated'
+      sign_in @user
       redirect_to @user
     else
       render 'edit'
@@ -63,13 +67,14 @@ class UsersController < ApplicationController
     end
 
     # Before filters
+    
+    # def signed_in_user
+    #   unless signed_in?
+    #     store_location
+    #     redirect_to signin_url, notice: "Please sign in."
+    #   end
+    # end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
