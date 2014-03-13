e415fa3f97f0735c31c4eb95be08efdff5e672d4
Thomas Hervey <h46@umbc.edu>
2014-03-13 14:54:37 -0400

    working on CH11 exercises. Completed exercises 2, 3... working on 1 and 4. Need to figure out why there is no longer a feed on profile page.

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -26,5 +26,34 @@ describe Relationship do
     before { relationship.follower_id = nil }
     it { should_not be_valid }
   end
-  
+
+  # 
+
+  # describe "relationship association" do
+  #   let(:other_user) { FactoryGirl.create(:user) }
+  #   before do
+  #     @user.save
+  #     @user.follow!(other_user)
+  #     other_user.follow!(@user)
+  #   end
+
+  #   it "should destroy associated relationships" do
+  #     relationships = @user.relationships
+  #     @user.destroy
+  #     expect(should).not_to be_empty
+  #     relationships.each do |relationship|
+  #       expect(Relationship.where(id: relationship.id)).to be_empty
+  #     end
+  #   end
+
+  #   it "should destroy associated reverse relationships" do
+  #     reverse_relationships = @user.reverse_relationships
+  #     @user.destroy
+  #     reverse_relationships.present?.should be_false
+  #   end
+
+  # end
 end
+
+
+    
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -116,6 +116,59 @@ describe "User pages" do
         end
       end
     end
+
+
+    # describe "user stats display correctly" do
+    #   let(:other_user) { FactoryGirl.create(:user) }
+    #   before do
+    #     user.follow!(other_user)
+    #     other_user.follow!(user)
+    #     visit user_path(user)
+    #   end
+
+    #   it { should have_selector('strong#following', text:'1') }
+    #   it { should have_selector('strong#followers', text:'1') }
+
+
+    #   describe "after unfollowing other user" do
+    #     before { user.unfollow!(other_user); visit user_path(user) }
+    #     it { should have_selector('strong#following', text: '0') }
+    #   end
+
+    #   describe "after unfollowed by other user" do
+    #     before { other_user.unfollow!(user); visit user_path(user) }
+    #     it { should have_selector('strong#followers', text: '0') }
+    #   end
+    # end
+
+
+
+    # describe "for signed-in users" do
+    #   let(:user) { FactoryGirl.create(:user) }
+    #   before do
+    #     FactoryGirl.create(:micropost, user: user, content: "Lorem")
+    #     FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+    #     sign_in user
+    #     visit root_path
+    #   end
+
+    #   it "should render the user's feed" do
+    #     user.feed.each do |item|
+    #       expect(page).to have_selector("li##{item.id}", text: item.content)
+    #     end
+    #   end
+
+    #   describe "follower/following counts" do
+    #     let(:other_user) { FactoryGirl.create(:user) }
+    #     before do
+    #       other_user.follow!(user)
+    #       visit root_path
+    #     end
+
+    #     it { should have_link("0 following", href: following_user_path(user)) }
+    #     it { should have_link("1 followers", href: followers_user_path(user)) }
+    #   end
+    # end
     
   end
 
